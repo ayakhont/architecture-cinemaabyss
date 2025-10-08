96d25e68b2bd1c31c3ebad9dcc3c7dcfe8bc3973
ayakhont <ayakhont@gmail.com>
2025-10-08 20:12:00 +0300

    Java working api proxy

--- src/microservices/proxy/app/build.gradle.kts
+++ src/microservices/proxy/app/build.gradle.kts
@@ -17,14 +17,18 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit test framework.
-    testImplementation("junit:junit:4.13.2")
-
-    // This dependency is used by the application.
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.6")
     implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:3.2.6")
+    implementation("org.springframework.boot:spring-boot-starter:3.2.6")
 }
 
 application {
     // Define the main class for the application.
-    mainClass.set("proxy.App")
+    mainClass.set("proxy.ProxyServiceApplication")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
